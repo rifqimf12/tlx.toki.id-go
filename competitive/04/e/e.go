a0c4db68c05aee06bd26fe3137ae204921ad66c0
rifqimf12 <rifqimf120@gmail.com>
2024-04-24 11:41:50 +0700

    add 04/e

---
+++ competitive/04/e/e.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+var (
+	m, n int
+	b    [25][25]int
+	bt   [25][25]bool
+)
+
+func look(l, r, c int) (count int) {
+	if r < 0 || c < 0 || r == m || c == n || bt[r][c] {
+		return
+	}
+	if l == b[r][c] {
+		bt[r][c] = true
+		count++
+		count += look(l, r+1, c)
+		count += look(l, r-1, c)
+		count += look(l, r, c+1)
+		count += look(l, r, c-1)
+	}
+	return
+}
+
+func main() {
+	fmt.Scanf("%d%d", &m, &n)
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			fmt.Scanf("%d", &b[i][j])
+		}
+	}
+	var max int
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if !bt[i][j] {
+				if t := look(b[i][j], i, j); t > max {
+					max = t
+				}
+			}
+		}
+	}
+	fmt.Printf("%d\n", max*(max-1))
+}
